f447e3f3483c03fc61a880619350d972d8cfb786
pengxianheng-xreal <xhpeng@xreal.com>
2024-04-10 13:58:41 +0800

    chore: compile success with visual studio 2022

--- simplest_video_play_sdl2/simplest_video_play_sdl2.cpp
+++ simplest_video_play_sdl2/simplest_video_play_sdl2.cpp
@@ -45,6 +45,17 @@
 
 #include <stdio.h>
 
+#if _MSC_VER>=1900
+#include "stdio.h" 
+_ACRTIMP_ALT FILE* __cdecl __acrt_iob_func(unsigned);
+#ifdef __cplusplus 
+extern "C"
+#endif 
+FILE * __cdecl __iob_func(unsigned i) {
+    return __acrt_iob_func(i);
+}
+#endif /* _MSC_VER>=1900 */
+
 extern "C"
 {
 #include "sdl/SDL.h"
